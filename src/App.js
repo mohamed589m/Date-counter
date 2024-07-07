13af7d6e4f6ff3b31a9ab9241a1786ca133ad957
mohamed589m <mohamed53634372@gmail.com>
2024-07-07 12:48:45 +0300

    Date Counter

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date("June 21 2027");
+  date.setDate(date.getDate() + count);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  return (
+    <div className="parent">
+      <div>
+        <button onClick={() => setStep((s) => (s > 1 ? s - 1 : 1))}>-</button>
+        <span className="counter">Step: {step}</span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span className="counter">Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      <button className="reset" onClick={handleReset}>Reset</button>
+    </div>
+  );
+}
